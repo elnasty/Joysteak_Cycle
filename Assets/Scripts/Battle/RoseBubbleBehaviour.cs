c1351e25a99f37d37284348e26c02adcad3a8651
KangSoon <tankangsoon@gmail.com>
2016-12-20 15:54:47 +0800

    Rose detects when player is within radius

---
+++ Assets/Scripts/Battle/RoseBubbleBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseBubbleBehaviour : MonoBehaviour {
+
+	public GameObject playerObj;
+	public float triggerRadius;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = (transform.position - playerObj.transform.position).magnitude;
+		if (distance <= triggerRadius) {
+			Debug.Log ("Rose bud activated");
+		}
+	}
+}
