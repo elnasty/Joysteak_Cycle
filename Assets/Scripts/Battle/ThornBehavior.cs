d1047f6a25e458c5bd962b1758563916687304e6
LNST\Le Buff <elnastyteo@gmail.com>
2016-12-22 17:15:52 +0800

    Thorn Projectile One Directional Rotation

--- Assets/Scripts/Battle/ThornBehavior.cs
+++ Assets/Scripts/Battle/ThornBehavior.cs
@@ -3,37 +3,54 @@ using System.Collections;
 
 public class ThornBehavior : MonoBehaviour
 {
-    public Transform target;
+    private GameObject target;
     public float velocity;
 
     private float turnspeed;
     private float homingSensitivity = 0.05f;
+    private bool clockwise;
     private bool fire = false;
 
 	// Use this for initialization
 	void OnEnable ()
     {
+        target = GameObject.FindGameObjectWithTag("Player");
+        clockwise = (Random.value > 0.5f);
+
         turnspeed = Random.Range(180f, 360f);
+
+        if (clockwise) turnspeed = -turnspeed;
+
         StartCoroutine(Spin(3f, turnspeed));
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         if (fire)
         {
             if (target != null)
             {
-                Vector2 relativePos = target.position - transform.position;
-                Debug.Log(relativePos);
-                Quaternion rotation = Quaternion.LookRotation(Vector3.forward, relativePos);
+                Vector2 relativePos = target.GetComponent<Transform>().position - transform.position;
+                float angle = Vector2.Angle(transform.up, relativePos);
+                float halfAngle;
+                Vector3 cross = Vector3.Cross(transform.up, relativePos);
+
+                if (cross.z < 0) angle = -angle;
+
+                if ((angle > 0) == clockwise) halfAngle = 180 + (angle) / 2;
+                else halfAngle = (angle) / 2;
+
+                Vector3 halfPos = RotateVector2d(transform.up, halfAngle);
+                Quaternion rotation = Quaternion.LookRotation(Vector3.forward, halfPos);
                 rotation.x = 0.0f;
                 rotation.y = 0.0f;
 
                 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, homingSensitivity);
             }
 
-            transform.Translate(Vector3.up * velocity * Time.deltaTime, Space.Self);
+            transform.Translate(Vector2.up * velocity * Time.deltaTime, Space.Self);
         }
     }
 
@@ -59,5 +76,14 @@ public class ThornBehavior : MonoBehaviour
         } while (currentTime < time);
 
         fire = true;
+    }
+
+    Vector2 RotateVector2d(Vector2 vec2, float degrees)
+    {
+        degrees *= Mathf.Deg2Rad;
+        
+        var x = vec2.x * Mathf.Cos(degrees) - vec2.y * Mathf.Sin(degrees);
+        var y = vec2.x * Mathf.Sin(degrees) + vec2.y * Mathf.Cos(degrees);
+        return new Vector2(x,y);
     }
 }
